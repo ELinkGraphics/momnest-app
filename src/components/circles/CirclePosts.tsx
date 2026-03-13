f4627d17554a1925f51b8859c472b413188552b7
ELinkGraphics <elinkgraphics@gmail.com>
2026-03-13 13:18:38 +0300

    Fix useState ReferenceError in CirclePosts.tsx

--- src/components/circles/CirclePosts.tsx
+++ src/components/circles/CirclePosts.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEditor, EditorContent } from '@tiptap/react';
 import { BubbleMenu } from '@tiptap/react/menus';
